ea5b3524c7ba246d504a14362868699ad707d3cc
buketculfaoglu <buketculfaoglu@gmail.com>
2024-08-08 20:25:43 +0300

    basic structure of the project

---
+++ BookYourStay.Infastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookYourStay.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookYourStay.Infastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
