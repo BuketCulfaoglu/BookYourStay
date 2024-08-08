7359a11d1028e93a7aa3f8f31294692d8fb0c9e6
buketculfaoglu <buketculfaoglu@gmail.com>
2024-08-09 01:04:54 +0300

    VillaController and Create page added dark theme applied

--- BookYourStay.Domain/Entities/Villa.cs
+++ BookYourStay.Domain/Entities/Villa.cs
@@ -1,11 +1,15 @@
-﻿namespace BookYourStay.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookYourStay.Domain.Entities
 {
     public class Villa
     {
         public int Id { get; set; }
         public required string Name { get; set; }
         public string? Description { get; set; }
+        [Display(Name="Image Url")]
         public string? ImageUrl { get; set; }
+        [Display(Name = "Price per night")]
         public double Price { get; set; }
         public int Sqft { get; set; }
         public int Occupancy { get; set; }
---
+++ BookYourStay.Web/Controllers/VillaController.cs
@@ -0,0 +1,40 @@
+﻿using BookYourStay.Domain.Entities;
+using BookYourStay.Infastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookYourStay.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var villas = _context.Villas.ToList();
+            return View(villas);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Villa villa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Add(villa);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+    }
+}
