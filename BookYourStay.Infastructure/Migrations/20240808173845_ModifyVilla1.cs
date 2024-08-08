7b266992d3840f9bf7f00f1f839da3d20b5ac598
buketculfaoglu <buketculfaoglu@gmail.com>
2024-08-08 20:41:33 +0300

    add migration (AddVillaToDb) modify Villa - imageUrl seed some data to Villas table

---
+++ BookYourStay.Infastructure/Migrations/20240808173845_ModifyVilla1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BookYourStay.Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyVilla1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "CreatedDate", "Description", "ImageUrl", "Name", "Occupancy", "Price", "Sqft", "UpdatedDate" },
+                values: new object[,]
+                {
+                    { 1, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x400", "Royal Villa", 4, 200.0, 550, null },
+                    { 2, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x401", "Premium Pool Villa", 4, 300.0, 550, null },
+                    { 3, null, "Fusce 11 tincidunt maximus leo, sed scelerisque massa auctor sit amet. Donec ex mauris, hendrerit quis nibh ac, efficitur fringilla enim.", "https://placehold.co/600x402", "Luxury Pool Villa", 4, 400.0, 750, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Villas");
+        }
+    }
+}
