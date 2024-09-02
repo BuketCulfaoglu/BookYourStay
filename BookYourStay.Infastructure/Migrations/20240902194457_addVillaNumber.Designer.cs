faa8ea309901a84526f940a06d688d46849a233b
buketculfaoglu <buketculfaoglu@gmail.com>
2024-09-02 22:51:03 +0300

    Create VillaNumber table and seed records

--- BookYourStay.Infastructure/Migrations/20240902194457_addVillaNumber.Designer.cs
+++ BookYourStay.Infastructure/Migrations/20240902194457_addVillaNumber.Designer.cs
@@ -4,6 +4,7 @@ using BookYourStay.Infastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BookYourStay.Infastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240902194457_AddVillaNumber")]
+    partial class AddVillaNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
