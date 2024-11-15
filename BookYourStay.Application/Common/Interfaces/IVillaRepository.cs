103b8c151fd5644027d00c3c903bfc525df2b953
buketculfaoglu <buketculfaoglu@gmail.com>
2024-11-15 11:07:25 +0300

    VillaRepository implementation part 1

--- BookYourStay.Application/Common/Interfaces/IVillaRepository.cs
+++ BookYourStay.Application/Common/Interfaces/IVillaRepository.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace BookYourStay.Application.Common.Interfaces
 {
-    internal interface IVillaRepository
+    public interface IVillaRepository
     {
         IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null);
         IEnumerable<Villa> Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null);
--- BookYourStay.Web/Program.cs
+++ BookYourStay.Web/Program.cs
@@ -1,4 +1,6 @@
+using BookYourStay.Application.Common.Interfaces;
 using BookYourStay.Infastructure.Data;
+using BookYourStay.Infastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
