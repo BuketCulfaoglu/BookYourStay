5ef39bf340bd5fcf51a8f41ed6b42e5ff6f653da
buketculfaoglu <buketculfaoglu@gmail.com>
2026-02-08 23:37:48 +0300

    Booking Repo

---
+++ BookYourStay.Infrastructure/Repository/BookingRepository.cs
@@ -0,0 +1,21 @@
+﻿using BookYourStay.Application.Common.Interfaces;
+using BookYourStay.Domain.Entities;
+using BookYourStay.Infrastructure.Data;
+
+namespace BookYourStay.Infrastructure.Repository
+{
+    public class BookingRepository : Repository<Booking>, IBookingRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public void Update(Booking entity)
+        {
+            _context.Bookings.Update(entity);
+        }
+    }
+}
